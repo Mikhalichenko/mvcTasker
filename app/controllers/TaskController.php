7161dd1a28b308ae656acf34c2dff9c3792820e9
Evgeniy <MikhalichenkoEvgeniy@gmail.com>
2017-04-24 20:19:37 +0300

    add resize func

--- app/controllers/TaskController.php
+++ app/controllers/TaskController.php
@@ -96,6 +96,7 @@ class TaskController extends BaseController
         if (empty($errors)) {
 
             if (move_uploaded_file($img["tmp_name"], $target_file)) {
+                self::resize(self::DEFAULT_IMG_WIDTH, self::UPLOADER_PATH . $img['name'],  self::UPLOADER_PATH . $img['name']);
                 return true;
             } else {
                 $errors[] = "Не удалось загрузить картинку";
@@ -105,6 +106,58 @@ class TaskController extends BaseController
             return $errors;
         }
     }
+
+    /**
+     * @param $newWidth
+     * @param $targetFile
+     * @param $originalFile
+     * Resize img if width > DEFAULT_IMG_WIDTH
+     */
+    public static function resize($newWidth, $targetFile, $originalFile) {
+
+        $info = getimagesize($originalFile);
+
+        if ($info[0] > self::DEFAULT_IMG_WIDTH) {
+            $mime = $info['mime'];
+
+            switch ($mime) {
+                case 'image/jpeg':
+                    $image_create_func = 'imagecreatefromjpeg';
+                    $image_save_func = 'imagejpeg';
+                    $new_image_ext = 'jpg';
+                    break;
+
+                case 'image/png':
+                    $image_create_func = 'imagecreatefrompng';
+                    $image_save_func = 'imagepng';
+                    $new_image_ext = 'png';
+                    break;
+
+                case 'image/gif':
+                    $image_create_func = 'imagecreatefromgif';
+                    $image_save_func = 'imagegif';
+                    $new_image_ext = 'gif';
+                    break;
+
+                default:
+                    throw new Exception('Unknown image type.');
+            }
+
+            $img = $image_create_func($originalFile);
+            list($width, $height) = getimagesize($originalFile);
+
+            $newHeight = ($height / $width) * $newWidth;
+            $tmp = imagecreatetruecolor($newWidth, $newHeight);
+            imagecopyresampled($tmp, $img, 0, 0, 0, 0, $newWidth, $newHeight, $width, $height);
+
+            if (file_exists($targetFile)) {
+                unlink($targetFile);
+            }
+            $image_save_func($tmp, "$targetFile.$new_image_ext");
+        }
+
+    }
+
 
 
     /**
